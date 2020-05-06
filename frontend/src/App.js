0763be5202968846ea7ed68f85f11aaf9ecda4c1
brettlofendo <blofendo@gmail.com>
2020-05-05 21:19:24 -0500

    frontend stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import GroupList from './GroupList';
+import StudentView from './StudentView';
 import GroupEdit from './GroupEdit';
 import StudentSearch from './StudentSearch';
+import InstructorView from "./InstructorView";
 
 class App extends Component {
     render() {
@@ -12,10 +13,12 @@ class App extends Component {
             <Router>
                 <Switch>
                     <Route path='/' exact={true} component={Home}/>
-                    <Route path='/students' exact={true} component={GroupList}/>
+                    <Route path='/students' exact={true} component={StudentView}/>
                     <Route path='/students/search' component={StudentSearch}/>
                     <Route path='/students/:id' component={GroupEdit}/>
-
+                    <Route path='/instructors' exact={true} component={InstructorView}/>
+                    <Route path='/instructors/search' component={StudentSearch}/>
+                    <Route path='/instructors/:id' component={GroupEdit}/>
                 </Switch>
             </Router>
         )
